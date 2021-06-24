edb6dc7ef30a734bb61bce97016cde98c24c4729
Somi Hwang <somihwang@Somis-MacBook-Pro.local>
2021-06-24 09:26:51 +0900

    Add: 로그인, 메인 페이지 js 및 scss 파일 추가

--- src/pages/somi/Login/LoginSomi.js
+++ src/pages/somi/Login/LoginSomi.js
@@ -1,9 +1,114 @@
 import React from 'react';
+import '../../../styles/reset.scss';
+import './Login.scss';
+import { withRouter } from 'react-router-dom';
 
 class LoginSomi extends React.Component {
+  goToMain = () => {
+    this.props.history.push('/somi/main');
+  };
+
   render() {
-    return <div>Hi</div>;
+    return (
+      <div class="loginBodySomi">
+        <main className="loginMain">
+          <div className="phoneImg">
+            <div className="imgBox">
+              <img
+                className="phoneImgs visible"
+                src="/images/somi/imgbox1.jpeg"
+                alt="인스타그램 홍보 이미지1"
+              />
+              <img
+                className="phoneImgs"
+                src="/images/somi/imgbox2.jpeg"
+                alt="인스타그램 홍보 이미지2"
+              />
+              <img
+                className="phoneImgs"
+                src="/images/somi/imgbox3.jpeg"
+                alt="인스타그램 홍보 이미지3"
+              />
+              <img
+                className="phoneImgs"
+                src="/images/somi/imgbox4.jpeg"
+                alt="인스타그램 홍보 이미지4"
+              />
+              <img
+                className="phoneImgs"
+                src="/images/somi/imgbox5.jpeg"
+                alt="인스타그램 홍보 이미지5"
+              />
+            </div>
+          </div>
+          <article className="rightBox">
+            <div className="loginWrapper">
+              <div className="logo">
+                <h1
+                  className="loginTitle"
+                  style={{ fontFamily: 'Lobster, cursive' }}
+                >
+                  Westagram
+                </h1>
+              </div>
+              <form className="loginContainer">
+                <input
+                  className="idInput input"
+                  type="text"
+                  placeholder="전화번호, 사용자 이름 또는 이메일"
+                />
+                <input
+                  className="passInput input"
+                  type="password"
+                  placeholder="비밀번호"
+                />
+                <button className="button" onClick={this.goToMain}>
+                  log in
+                </button>
+              </form>
+              <div className="lineBox">
+                <div className="line"></div>
+                <div className="or">OR</div>
+                <div className="line"></div>
+              </div>
+              <button className="fb">
+                <i className="fab fa-facebook-square"></i>
+                <a href="#">
+                  <span>Log in with Facebook</span>
+                </a>
+              </button>
+              <div className="findPassword">
+                <a href="#">비밀번호를 잊으셨나요?</a>
+              </div>
+            </div>
+            <div className="signUp">
+              <p>Don't have an account? </p>
+              <a href="#">Sign up</a>
+            </div>
+            <div className="app">
+              <p>Get the app.</p>
+            </div>
+            <div className="appLink">
+              <a href="#">
+                <img
+                  className="appLinkBtn"
+                  src="/images/somi/app1.png"
+                  alt="애플스토어"
+                />
+              </a>
+              <a href="#">
+                <img
+                  className="appLinkBtn"
+                  src="/images/somi/app2.png"
+                  alt="플레이스토어"
+                />
+              </a>
+            </div>
+          </article>
+        </main>
+      </div>
+    );
   }
 }
 
-export default LoginSomi;
+export default withRouter(LoginSomi);
--- src/pages/somi/Main/MainSomi.js
+++ src/pages/somi/Main/MainSomi.js
@@ -1,8 +1,257 @@
 import React from 'react';
+import '../../../styles/reset.scss';
+import './Main.scss';
+import Nav from '../../../components/Nav/Nav';
+import '../../../components/Nav/Nav.scss';
 
 class MainSomi extends React.Component {
   render() {
-    return <div>Hi</div>;
+    return (
+      <div>
+        <Nav />
+        <div className="mainBodySomi">
+          <div className="container">
+            <main className="mainMain">
+              <article className="feeds">
+                <div className="feedTop">
+                  <img
+                    className="feedProfile"
+                    src="/images/somi/feedProfile.jpeg"
+                    alt="프로필"
+                  />
+                  <div className="feedTopRight">
+                    <a href="#" className="userID">
+                      somangoi
+                    </a>
+                    <i className="fas fa-ellipsis-h"></i>
+                  </div>
+                </div>
+                <div className="feedPhotoContainer">
+                  <div className="feedPicBox">
+                    <img
+                      className="feedPic"
+                      src="/images/somi/feedPic.jpeg"
+                      alt="업로드사진"
+                    />
+                  </div>
+                </div>
+                <div className="contentIcons">
+                  <div className="iconContainerLeft">
+                    <button className="contentBtn">
+                      <img src="/images/somi/heart.png" alt="heart_icon" />
+                    </button>
+                    <button className="contentBtn">
+                      <img src="/images/somi/comment.png" alt="comment_icon" />
+                    </button>
+                    <button className="contentBtn">
+                      <img src="/images/somi/share.png" alt="share_icon" />
+                    </button>
+                  </div>
+                  <div className="iconContainerRight">
+                    <button className="contentBtn">
+                      <img
+                        src="/images/somi/bookmark.png"
+                        alt="bookmark_icon"
+                      />
+                    </button>
+                  </div>
+                </div>
+                <div className="likes">
+                  <img
+                    className="likesPic"
+                    src="../images/somi/likeProfile.jpeg"
+                    alt="프로필"
+                  />
+                  <div className="likesDesc">
+                    <a href="#" className="bolder">
+                      gleitflug
+                    </a>
+                    님{' '}
+                    <a href="#" className="bolder">
+                      외 10명
+                    </a>
+                    이 좋아합니다.
+                  </div>
+                </div>
+                <ul className="commentList">
+                  <li className="commentBox">
+                    <span className="igID">
+                      <a href="#">somangoi</a>
+                    </span>
+                    <span className="commentContent">
+                      <span>역시 감성터지는 위코드</span>
+                    </span>
+                    <i className="deleteBtn fal fa-trash"></i>
+                  </li>
+                  <li className="commentBox">
+                    <span className="igID">
+                      <a href="#">bongbongcatcat</a>
+                    </span>
+                    <span className="commentContent">
+                      <span>위코드 쩐당</span>
+                    </span>
+                    <i className="deleteBtn fal fa-trash"></i>
+                  </li>
+                </ul>
+                <span className="commentTime">42분 전</span>
+                <form className="commentInputBox">
+                  <input
+                    className="commentInput"
+                    type="text"
+                    placeholder="댓글 달기..."
+                  />
+                  <button className="commentBtn" type="submit">
+                    Post
+                  </button>
+                </form>
+              </article>
+              <aside className="mainRight">
+                <div className="profile">
+                  <img
+                    className="manRightProfilePic"
+                    src="/images/somi/feedProfile.jpeg"
+                    alt="프로필"
+                  />
+                  <div className="profileDesc">
+                    <span className="profileID">somangoi</span>
+                    <span className="profileName">Somi Hwang</span>
+                  </div>
+                </div>
+                <div className="storyBox">
+                  <div className="storyTop">
+                    <span className="smallTitle">스토리</span>
+                    <span className="seeAll">모두 보기</span>
+                  </div>
+                  <ul className="stories">
+                    <li className="story">
+                      <canvas className="circle"></canvas>
+                      <a href="#">
+                        <img
+                          className="storyPic sideProfile"
+                          src="/images/somi/story1.jpeg"
+                          alt="스토리 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">abcd</span>
+                        <span className="storyTime">16분 전</span>
+                      </div>
+                    </li>
+                    <li className="story">
+                      <canvas className="circle"></canvas>
+                      <a href="#">
+                        <img
+                          className="storyPic sideProfile"
+                          src="/images/somi/story2.jpeg"
+                          alt="스토리 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">efg</span>
+                        <span className="storyTime">20분 전</span>
+                      </div>
+                    </li>
+                    <li className="story">
+                      <canvas className="circle"></canvas>
+                      <a href="#">
+                        <img
+                          className="storyPic sideProfile"
+                          src="/images/somi/story3.jpeg"
+                          alt="스토리 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">hijk</span>
+                        <span className="storyTime">1시간 전</span>
+                      </div>
+                    </li>
+                    <li className="story">
+                      <canvas className="circle"></canvas>
+                      <a href="#">
+                        <img
+                          className="storyPic sideProfile"
+                          src="/images/somi/story4.jpeg"
+                          alt="스토리 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">lmnop</span>
+                        <span className="storyTime">20시간 전</span>
+                      </div>
+                    </li>
+                  </ul>
+                </div>
+                <div className="recommendationBox">
+                  <div className="recommendationTop">
+                    <span className="smallTitle">회원님을 위한 추천</span>
+                    <span className="seeAll">모두 보기</span>
+                  </div>
+                  <ul className="recommendations">
+                    <li className="recommendation">
+                      <a href="#">
+                        <img
+                          className="sideProfile"
+                          src="/images/somi/story1.jpeg"
+                          alt="추천 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">abcd</span>
+                        <div className="connection">
+                          wecode_bootcamp님 외 1명이 팔로우 하고 있습니다.
+                        </div>
+                      </div>
+                      <button clas="followBtn">팔로우</button>
+                    </li>
+                    <li className="recommendation">
+                      <a href="#">
+                        <img
+                          className="sideProfile"
+                          src="/images/somi/story2.jpeg"
+                          alt="추천 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">efg</span>
+                        <div className="connection">
+                          bongbongcatcat님 외 1명이 팔로우 하고 있습니다.
+                        </div>
+                      </div>
+                      <button clas="followBtn">팔로우</button>
+                    </li>
+                    <li className="recommendation">
+                      <a href="#">
+                        <img
+                          className="sideProfile"
+                          src="/images/somi/story3.jpeg"
+                          alt="추천 프로필"
+                        />
+                      </a>
+                      <div className="sideDesc">
+                        <span className="sideId">hijk</span>
+                        <div className="connection">
+                          kimchi_chips님 외 1명이 팔로우 하고 있습니다.
+                        </div>
+                      </div>
+                      <button clas="followBtn">팔로우</button>
+                    </li>
+                  </ul>
+                </div>
+                <footer>
+                  <p>
+                    Instagram 정보 &middot; 지원 &middot; 홍보 센터 &middot; API
+                    &middot; 채용 정보 &middot; 개인정보처리방침 &middot; 약관
+                    &middot; 디렉터리 &middot; 프로필 &middot; 해시태그 &middot;
+                    언어
+                  </p>
+                  <p className="copyright">&copy; 2021 INSTAGRAM</p>
+                </footer>
+              </aside>
+            </main>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
