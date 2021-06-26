be23c0e85697e3319433db9d4e5786e31e704b3b
Somi Hwang <somihwang@Somis-MacBook-Pro.local>
2021-06-27 08:08:38 +0900

    Add: 댓글 삭제 기능 구현

--- src/pages/somi/Comment/Comment.js
+++ src/pages/somi/Comment/Comment.js
@@ -1,17 +1,51 @@
 import React from 'react';
 
 class Comment extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      likeColor: true,
+    };
+  }
+
+  deleteComment = e => {
+    const item = e.target;
+    const parentItem = item.parentElement;
+
+    if (item.className[0] === 'd') {
+      parentItem.remove();
+    }
+  };
+
   render() {
     return (
       <>
+        <li className="commentBox">
+          <span className="igID">
+            <a href="#">bongbongcatcat</a>
+          </span>
+          <span className="commentContent">위코드 쩐당</span>
+          <i className="likeBtn far fa-heart" onClick={this.likeComment}></i>
+          <i
+            className="deleteBtn fas fa-times"
+            onClick={this.deleteComment}
+          ></i>
+        </li>
         {this.props.commentList.map(comment => (
           <li className="commentBox">
             <span className="igID">
               <a href="#">{this.props.profileID}</a>
             </span>
             <span className="commentContent">{comment}</span>
-            <i className="likeBtn far fa-heart"></i>
-            <i className="deleteBtn fas fa-times"></i>
+            <i
+              className="likeBtn far fa-heart"
+              onClick={this.likeComment}
+              style={{ color: this.state.likeColor ? 'black' : 'red' }}
+            ></i>
+            <i
+              className="deleteBtn fas fa-times"
+              onClick={this.deleteComment}
+            ></i>
           </li>
         ))}
       </>
