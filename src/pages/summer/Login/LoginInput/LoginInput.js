1367abc5613f688ede09fdb61a38ce19f1986961
kylee <anachrosh@gmail.com>
2021-06-29 14:42:01 +0900

    MODI: 로그인 input 합침

--- src/pages/summer/Login/LoginInput/LoginInput.js
+++ src/pages/summer/Login/LoginInput/LoginInput.js
@@ -16,43 +16,55 @@ class LoginInput extends React.Component {
     this.props.history.push('/summer/main');
   };
 
-  handleIdInput = e => {
-    this.setState({ idInput: e.target.value });
-    const checkIdVld = /^(?=.*[@])[a-z0-9_.@]{5,}$/gi;
-    const checkedId = checkIdVld.test(e.target.value);
-    checkedId === true
-      ? this.setState({ idVld: true })
-      : this.setState({ idVld: false });
+  handleInput = e => {
+    const { value, name } = e.target;
+
+    this.setState({
+      [name]: value,
+    });
   };
 
-  handlePwInput = e => {
-    this.setState({ pwInput: e.target.value });
+  checkLoginVld = () => {
+    const { idInput, pwInput } = this.state;
+
+    const checkIdVld = /^(?=.*[@])[a-z0-9_.@]{5,}$/gi;
+    const checkedId = checkIdVld.test(idInput);
     const checkPwVld = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
-    const checkedPW = checkPwVld.test(e.target.value);
-    checkedPW === true
+    const checkedPw = checkPwVld.test(pwInput);
+
+    checkedId
+      ? this.setState({ idVld: true })
+      : this.setState({ idVld: false });
+    checkedPw
       ? this.setState({ pwVld: true })
       : this.setState({ pwVld: false });
   };
+
   render() {
     const { idVld, pwVld } = this.state;
+    const { handleInput, gotoMain, checkLoginVld } = this;
 
     return (
       <form action="#" method="POST" name="user-login">
         <input
-          onChange={this.handleIdInput}
+          onChange={handleInput}
+          onKeyUp={checkLoginVld}
           type="text"
           placeholder="사용자 이름 또는 이메일"
           id="id-input"
+          name="idInput"
         />
         <input
-          onChange={this.handlePwInput}
+          onChange={handleInput}
+          onKeyUp={checkLoginVld}
           type="password"
           placeholder="비밀번호"
           id="pw-input"
+          name="pwInput"
         />
         {idVld && pwVld ? (
           <button
-            onClick={this.gotoMain}
+            onClick={gotoMain}
             style={{ background: '#0095F6' }}
             type="submit"
           >
